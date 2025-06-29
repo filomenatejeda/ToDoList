2e9a730476895012ddd923f676f252d3901c530b
RobTamGon <Alotamgon@gmail.com>
2025-06-29 04:10:20 -0400

    Cambio en tiempo real sin actualizar la página
    REVISAR: los botones de "Marcar como...", ¿qué hacemos con ellos?

--- static/js/toggle.js
+++ static/js/toggle.js
@@ -1,7 +1,14 @@
 function toggleSubtarea(checkbox) {
   fetch(checkbox.dataset.url, {
     method: "POST"
-  }).catch(error => {
+  })
+  .then(response => response.json())
+  .then(data => {
+	const nuevo_estado = data.nuevo_estado;
+
+	document.querySelector(".estado-tarea").textContent = nuevo_estado.charAt(0).toUpperCase() + nuevo_estado.slice(1);
+  })
+  .catch(error => {
     console.error("Error toggling subtarea:", error);
   });
 }
